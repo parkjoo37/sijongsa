4d33a68da400e2c74a6723ae08b8667601b09a52
parkjoo37 <parkjoo370@naver.com>
2020-07-01 20:05:48 +0900

    layout
    레이아웃 나누기

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <v-layout wrap row id="navbar">
+    <v-flex id="navbar__menu" xs12>
+      <a :href="item.path" v-for="item in items" :key="item">{{ item.title }}</a>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: '반지', path: '/ring' },
+        { title: '목걸이', path: '/necklace' },
+        { title: '팔찌', path: '/bracelet' },
+        { title: '귀걸이', path: '/earring' },
+        { title: '시계', path: '/clock' }
+      ]
+    };
+  }
+}
+</script>
+
+<style lang="css" src="../assets/css/navbar.css" scoped>
+</style>
